6bea5df80f2a3088fbd57f7a4424d224c887b4cb
FeiPF2020 <wpfn218925@163.com>
2022-10-01 18:43:36 +0800

    Changes

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include <Eigen/Core>
+
+using namespace  Eigen;
+using namespace  std;
+
+
+
+int main() {
+    Vector3d V{1,2,3};
+    cout<< V<<endl;
+
+
+    cout<<" wpf yes! "<<endl;
+
+    return 0;
+}
